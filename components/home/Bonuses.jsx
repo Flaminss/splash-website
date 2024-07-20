0c4f08cd89e2b6a6ef8f6e9d3764771bcb535ddf
noahdimattina <noahdimattina@gmail.com>
2024-07-20 19:40:00 -0400

    fix

--- components/home/Bonuses.jsx
+++ components/home/Bonuses.jsx
@@ -14,24 +14,9 @@ export default function Bonuses() {
 
         <div className="card__area">
           <div className="cards bonuses">
-            <div className="card card-top">
-              <div className="title">
-                <img src="/images/csgo-roll.png" alt="CSGO Roll" className="title__img" /> {/* Replace text with image */}
-              </div>
-              <div className="rewards">
-                <h3>Rewards</h3>
-                <p>DAILY FREE CASES + RAKEBACK</p>
-                <p>5% DEPOSIT BONUS</p>
-                <p>DAILY DEPOSIT GIVEAWAYS</p>
-              </div>
-              <p className="use__code">Use Code</p>
-              <div className="card__btn">
-                <button className="btn__card">SplashCS</button>
-              </div>
-            </div>
-            <div className="card card-bottom">
-              <div className="title">
-                <img src="/images/packdraw.png" alt="Packdraw" className="title__img" /> {/* Replace text with image */}
+            <div className="card card-packdraw">
+              <div className="title title-packdraw">
+                <img src="/images/packdraw.png" alt="Packdraw" className="title__img" />
               </div>
               <div className="rewards">
                 <h3>Rewards</h3>
@@ -44,9 +29,24 @@ export default function Bonuses() {
                 <button className="btn__card">Splash</button>
               </div>
             </div>
-            <div className="card card-top">
-              <div className="title">
-                <img src="/images/clash.png" alt="Clash" className="title__img" /> {/* Replace text with image */}
+            <div className="card card-csgo-roll">
+              <div className="title title-csgo-roll">
+                <img src="/images/csgo-roll.png" alt="CSGO Roll" className="title__img" />
+              </div>
+              <div className="rewards">
+                <h3>Rewards</h3>
+                <p>DAILY FREE CASES + RAKEBACK</p>
+                <p>5% DEPOSIT BONUS</p>
+                <p>DAILY DEPOSIT GIVEAWAYS</p>
+              </div>
+              <p className="use__code">Use Code</p>
+              <div className="card__btn">
+                <button className="btn__card">SplashCS</button>
+              </div>
+            </div>
+            <div className="card card-clash">
+              <div className="title title-clash">
+                <img src="/images/clash.png" alt="Clash" className="title__img" />
               </div>
               <div className="rewards">
                 <h3>Rewards</h3>
